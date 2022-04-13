eea95ddb77b3ba3441fadceed1d410ca9bf98681
Schuyler Erle <schuyler@lattice.com>
2022-04-13 09:18:16 -0700

    Move {gen,del}-rule.sh into download/ and convert minutes to seconds

--- download/gen-rule.sh
+++ download/gen-rule.sh
@@ -17,7 +17,7 @@ cat >tmp.json <<EOF
     {
         "Id": "1",
         "Arn": "arn:aws:lambda:${REGION}:${ACCOUNT}:function:${FUNCTION}",
-        "Input": "{\"time\": \"${HOUR}:${MIN}\", \"duration\": ${DURATION}}",
+        "Input": "{\"time\": \"${HOUR}:${MIN}\", \"duration\": $((${DURATION} * 60))}",
         "RetryPolicy": {
             "MaximumEventAgeInSeconds": 60
         }
